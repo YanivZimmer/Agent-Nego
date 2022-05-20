63c907fcee3a53b6bee827ef51c3acda52b5a9e3
YanivZimmer <zimmer.yaniv@gmail.com>
2022-05-20 15:40:12 +0300

    Persistent_data::get_smooth_thresh,calc_alpha

--- agents/super_agent/utils/persistent_data.py
+++ agents/super_agent/utils/persistent_data.py
@@ -10,7 +10,7 @@ class PersistentData(ABC):
         self._t_split: int = 40
         self._t_phase: float = 0.2
         self._new_weight: float = 0.3
-        self._smooth_width: float = 3
+        self._smooth_width: int = 3
         self._opponent_decrease: float = 0.65
         self._default_alpha: float = 10.7
 
@@ -73,5 +73,47 @@ class PersistentData(ABC):
         self._opponent_utility_by_time[opponent] = opponent_time_util
         self._opponent_alpha[opponent] = self._calc_alpha(opponent)
 
-    def _calc_alpha(opponent: str):
-        pass
+    def _known_opponent(self, opponent: str):
+        return self._opponent_encounters.keys().__contains__(opponent)
+
+    def _get_smooth_threshold_over_time(self, opponent: str):
+        if not self._known_opponent(opponent):
+            return None
+
+        opponent_time_util = self._opponent_utility_by_time.get(opponent)
+        smoothed_time_util = list()
+        for i in range(self._t_split):
+            smoothed_time_util[i] = 0.0
+
+        for i in range(self._t_split):
+            for j in range(max(0, i-self._smooth_width), min(i+self._smooth_width+1,self._t_split)):
+                smoothed_time_util[i] += opponent_time_util[j]
+            smoothed_time_util[i] /= (min(i+self._smooth_width+1,self._t_split)-max(i-self._smooth_width,0))
+
+        return smoothed_time_util
+
+    def _calc_alpha(self, opponent: str):
+        alpha_arr=self._get_smooth_threshold_over_time(opponent)
+        if alpha_arr is None:
+            return self._default_alpha
+        max_idx=0
+        t=0
+        for max_idx in range(self._t_split):
+            if alpha_arr[max_idx]>0.2:
+                break
+        max_val = alpha_arr[0]
+        min_val = alpha_arr[max(max_idx-self._smooth_width-1, 0)]
+        if max_val - min_val < 0.1:
+            return self._default_alpha
+        for t in range(max_idx):
+            if alpha_arr[t]>(max_val-self._opponent_decrease*(max_val-min_val)):
+                break
+        calibrated_polynom = [572.83, -1186.7, 899.29, -284.68, 32.911]
+        alpha = calibrated_polynom[0]
+        t_time = self._t_phase+(1-self._t_phase)*\
+                 (max_idx*(t/self._t_split)+\
+                  ((self._t_split-max_idx)*0.85)/self._t_split)
+        for i in range(1,len(calibrated_polynom)):
+            alpha = alpha*t_time + calibrated_polynom[i]
+        print("alpha={0}".format(alpha))
+        return alpha
